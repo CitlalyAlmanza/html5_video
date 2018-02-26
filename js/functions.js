4fe77638a9ea204ef3f76cf696ddce33f95b50a6
gaabrielaagm <monserratgm>
2018-02-25 19:49:41 -0600

    Ultima modificación

--- js/functions.js
+++ js/functions.js
@@ -1,39 +1,9 @@
-/*
-function clic(){
-	$("ul li").on("click", values())
-}
 
-function() values() {
-	$("video").attr({
-	"src": $(this).attr("movieurl"),
-	"poster": $(this).attr("moviesposter"),
-	"autoplay": "autoplay",
-	"mute": "mute"
-	});
-}
-*/
-
-/*
 $(function() {
-    $("#playlist li").on("click", function() {
-        $("#videoarea").attr({
-            src: $("#playlist li").attr("movieurl"),
-            poster: $("#playlist li").attr("moviesposter"),
-            muted: "muted"
-        })
-    })
-})
-*/
-$(function() {
-    $("li video").on("click", function() {
+    $(document).on("click", "li video", function() { 
         $("#videoarea").attr({
             "src": $(this).attr("src"),
             "poster": $(this).attr("poster")
         })
-    })
-    
-    $("#videoarea").attr({
-        "src": $("li video").eq(0).attr("src"),
-        "poster": $("li video").eq(0).attr("poster")
-    })
+    })    
 })
